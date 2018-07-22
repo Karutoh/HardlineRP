b117c0e028578bfa3a52f72f2e4258a6cbda15ed
Arron David Nelson <red-xiii@outlook.com>
2018-07-22 01:14:25 -0700

    Added modular f4 menu.

--- gamemode/f4menu/client.lua
+++ gamemode/f4menu/client.lua
@@ -1,266 +1,87 @@
-local tabs = {
-	
-	{
-		name = "Jobs"
-	},
-	{
-		name = "filler"
-	},
-	{
-		name = "filler2"
-	}
+local menuActive = false
+local currentTab = 1
+local tabs = {}
 
-}
+local function ShowTab(panel, index)
+	local children = panel:GetChildren()
+	for i = 1, #children do
+		children[i]:Remove()
+	end
 
-local jobstbl = {}
-
-local isCreated = false
-local isVisible = false
-
-local PANEL = {}
-PANEL.width = 800
-PANEL.height = 600
-PANEL.currentTab = 1
-
-function PANEL:Init()
-	self:SetPos((ScrW() / 2) - (self.width / 2), (ScrH() / 2) - (self.height / 2)) 
-	self:SetSize(self.width, self.height)
-
-	self.tabPanel = vgui.Create("DPanel", self)
-	self.tabPanel:SetPos(0, 20)
-	self.tabPanel:SetSize(200, self.height - 20)
-
-	self.sheet = vgui.Create("DPanel", self)
-	self.sheet:SetPos(201, 20)
-	self.sheet:SetSize(self.width - 201, self.height - 20)
-
-	self.displayPanel = vgui.Create( "DScrollPanel", self.sheet )
-	self.displayPanel:SetSize(self.width - 201, self.height - 20)
-
-	tabContent(self)
-	setupDisplay(self, 1)
+	tabs[index].cb(panel)
 end
 
-
-function PANEL:Paint(w, h)
-	if !isVisible then return false end
-	draw.RoundedBox(0, 0, 0, w, h, Color(20, 20, 20, 150))
-	
-	header(self)
-
-
-	self.sheet.Paint = function()
-		if !isVisible then return false end
-		draw.RoundedBox(0, 0, 0, self.sheet:GetWide(), self.sheet:GetTall(), Color(220, 220, 220, 255))
+function TRP.AddF4MenuTab(title, cb)
+	for i = 1, #tabs do
+		if tabs[i].title == title then
+			return false
+		end
 	end
 
-	self.tabPanel.Paint = function()
-	if !isVisible then return false end
-		draw.RoundedBox(0, 0, 0, self.tabPanel:GetWide(), self.tabPanel:GetTall(), Color(20, 20, 20, 180))
-	end
+	table.insert(tabs, {title = title, cb = cb})
 
-	self.displayPanel.Paint = function()
-		if !isVisible then return false end
-		draw.RoundedBox(0, 0, 0, self.displayPanel:GetWide(), self.displayPanel:GetTall(), Color(20, 20, 20, 180))
-	end
-
-	local sbar = self.displayPanel:GetVBar()
-
-	sbar.Paint = function()
-		if !isVisible then return false end
-		draw.RoundedBox( 0, 0, 0, w, h, Color( 0, 0, 0, 100 ) )
-	end
-
-	sbar.btnUp.Paint = function()
-		if !isVisible then return false end
-
-		draw.RoundedBox( 0, 0, 0, w, h, Color( 200, 200, 200, 100 ) )
-	end
-
-	sbar.btnDown.Paint = function()
-		if !isVisible then return false end
-		draw.RoundedBox( 0, 0, 0, w, h, Color( 200, 200, 200, 100 ) )
-	end
-
-	sbar.btnGrip.Paint = function()
-		if !isVisible then return false end
-		draw.RoundedBox( 0, 0, 0, w, h, Color( 255, 255, 255, 255 ) )
-	end
+	return true
 end
 
-function header(self)
+net.Receive("TRP_OpenF4Menu", function (len, ply)
+	if menuActive then
+		return
+	end
 
-	surface.SetDrawColor(0, 0, 0, 255)
-	surface.DrawRect(0, 0, self.width, 20)
+	menuActive = true
 
-	surface.SetFont("headerFont")
-	surface.SetTextColor(255, 255, 255, 255)
-	surface.SetTextPos(10, 2)
-	surface.DrawText("F4 Menu")
-end
-
-function tabContent(PANEL)
+	gui.EnableScreenClicker(true)
 	
-	local button
+	local f4Menu = vgui.Create("DFrame")
+	f4Menu:SetSize(ScrW() / 1.25, ScrH() / 1.25)
+	f4Menu:Center()
+	f4Menu:SetTitle("Primary Menu")
+	f4Menu:SetDraggable(true)
+	f4Menu:SetDeleteOnClose(true)
+
+	function f4Menu:OnClose()
+		gui.EnableScreenClicker(false)
+		menuActive = false
+	end
+
+	local panel = vgui.Create("DPanel", f4Menu)
+	panel:SetSize(f4Menu:GetWide() * 0.75 - 5, f4Menu:GetTall() - 35)
+	panel:SetPos(f4Menu:GetWide() / 4, 30)
+
+	local pad = 5
 
 	for i = 1, #tabs do
-		button = vgui.Create("DButton", PANEL.tabPanel)
-		button:SetSize(PANEL.tabPanel:GetWide(), 50)
-		button:SetPos(0, (i - 1) * 50)
-		button:SetText("")
+		local tab = vgui.Create("DButton", f4Menu)
+		tab:SetSize(f4Menu:GetWide() / 4 - pad * 2, 30)
+		tab:SetPos(5, (30 + pad) * (i - 1) + pad + 25)
+		tab:SetText(tabs[i].title)
 
-		button.DoClick = function()
-
-		PANEL.currentTab = i
-
-		setupDisplay(PANEL, i)
-
+		tab.DoClick = function ()
+			ShowTab(panel, i)
 		end
 
-		button.Paint = function()
-		
-			if !isVisible then return false end
-
-			draw.RoundedBox(0, 0, 0, button:GetWide(), button:GetTall(), Color(0, 0, 0, 255))
-			draw.RoundedBox(0, 1, 1, button:GetWide() - 2, button:GetTall() - 2, Color(220, 220, 220, 255))
-
-			surface.SetFont("buttonFont")
-			surface.SetTextColor(0, 0, 0, 255)
-			local tWidth, tHeight = surface.GetTextSize(tabs[i]['name'])
-			surface.SetTextPos((button:GetWide() / 2)-(tWidth / 2), (button:GetTall() / 2) - (tHeight / 2))
-			surface.DrawText(tabs[i]['name'])
+		if i == currentTab then
+			ShowTab(panel, i)
 		end
 	end
-	
-end
+end)
 
-function setupDisplay(PANEL, tabIndex)
-	PANEL.displayPanel:Clear()
+TRP.AddF4MenuTab("Jobs", function (panel)
+	local button = vgui.Create("DButton", panel)
+	button:SetText("Hello World")
+	button:SetSize(100, 30)
+end)
 
-	if tabs[tabIndex]['name'] == "Jobs" then
-		if jobstbl == nil || #jobstbl == 0 then
-			local nojobsText = vgui.Create("DPanel", PANEL.displayPanel)
-			nojobsText:SetSize(PANEL.displayPanel:GetWide(), 150)
-			nojobsText:SetPos(0, 0)
+TRP.AddF4MenuTab("Test #1", function (panel)
+	local button = vgui.Create("DButton", panel)
+	button:SetText("Fuck You")
+	button:SetSize(100, 30)
+	button:SetPos(100, 100)
+end)
 
-			nojobsText.Paint = function()
-				if !isVisible then return false end
-
-				local nojobs = "There are currently no jobs that can be found!";
-
-				surface.SetFont("buttonFont")
-				local tWidth, tHeight = surface.GetTextSize(nojobs)
-
-				surface.SetTextColor(0, 0, 0, 255)
-				surface.SetTextPos((PANEL.displayPanel:GetWide() / 2) - (tWidth / 2), 20)
-				surface.DrawText(nojobs)
-			end
-		else
-			for i = 1, #jobstbl do
-				for j = 1, #jobstbl[i].jobTitles do
-					for r = 1, #jobstbl[i].jobTitles[j].jobRanks do
-						local jobPanel = vgui.Create("DButton", PANEL.displayPanel)
-						jobPanel:SetText("")
-						jobPanel:SetSize(PANEL.displayPanel:GetWide(), 150)
-						jobPanel:SetPos(0, (i - 1) * jobPanel:GetTall())
-
-						local jobDescription = vgui.Create("DButton", jobPanel)
-						jobDescription:SetText("")
-						jobDescription:SetSize(jobPanel:GetWide() / 2, jobPanel:GetTall() / 2)
-						jobDescription:SetPos((jobPanel:GetWide() / 2) - (jobDescription:GetWide() / 2), jobPanel:GetTall() - jobDescription:GetTall())
-
-						local jdLabel = vgui.Create("DLabel", jobDescription)
-						jdLabel:SetAutoStretchVertical(true);
-						jdLabel:SetWide(jobDescription:GetWide());
-						jdLabel:SetWrap(true);
-						jdLabel:SetMouseInputEnabled(true)
-						jdLabel:SetCursor("hand")
-
-						jobPanel.Paint = function()
-							if !isVisible || tabs[tabIndex]['name'] != "Jobs" then return false end
-
-							if jobPanel:IsHovered() || jobDescription:IsHovered() || jdLabel:IsHovered() then
-								draw.RoundedBox(0, 0, 0, jobPanel:GetWide(), jobPanel:GetTall(), Color(255, 255, 255, 255))
-								draw.RoundedBox(0, 2, 2, jobPanel:GetWide()-4, jobPanel:GetTall()-4, Color(80, 80, 80, 255))
-							else
-								draw.RoundedBox(0, 0, 0, jobPanel:GetWide(), jobPanel:GetTall(), Color(0, 0, 0, 255))
-								draw.RoundedBox(0, 2, 2, jobPanel:GetWide()-4, jobPanel:GetTall()-4, Color(80, 80, 80, 255))
-							end
-
-							surface.SetFont("buttonFont")
-							surface.SetTextColor(0, 0, 0, 255)
-							local tWidth, tHeight = surface.GetTextSize(jobstbl[i].jobTitles[j].title)
-							surface.SetTextPos((jobPanel:GetWide() / 2) - (tWidth / 2), 5)
-							surface.DrawText(jobstbl[i].jobTitles[j].title)
-							surface.SetTextColor(0, 0, 0, 255)
-							local tWidth, tHeight = surface.GetTextSize(jobstbl[i].jobTitles[j].jobRanks[r].title)
-							surface.SetTextPos((jobPanel:GetWide() / 2) - (tWidth / 2), tHeight+5)
-							surface.DrawText(jobstbl[i].jobTitles[j].jobRanks[r].title)
-						end
-
-						jobPanel.DoClick = function()
-							jobPanelClick(jobstbl[i].title,jobstbl[i].jobTitles[j].title, jobstbl[i].jobTitles[j].jobRanks[r].title)
-						end
-
-						jobDescription.Paint = function()
-							if !isVisible then return false end
-						end
-
-						jobDescription.DoClick = function()
-							jobPanelClick(jobstbl[i].title,jobstbl[i].jobTitles[j].title, jobstbl[i].jobTitles[j].jobRanks[r].title)
-						end
-
-						jdLabel.Paint = function()
-							if !isVisible then 
-								jdLabel:SetText("")
-								return false 
-							end
-							jdLabel:SetFont("Default")
-							jdLabel:SetText(jobstbl[i].jobTitles[1].description)
-						end
-
-						jdLabel.DoClick = function()
-							jobPanelClick(jobstbl[i].title,jobstbl[i].jobTitles[j].title, jobstbl[i].jobTitles[j].jobRanks[r].title)
-						end
-
-					end
-
-				end
-
-			end
-		end
-
-	else
-
-	end
-end
-
-function jobPanelClick(category, title, rank)
-	net.Start("TRP_F4MenuSetJob")
-		net.WriteString(category)
-		net.WriteString(title)
-		net.WriteString(rank)
-	net.SendToServer()
-end
-
-function showMenu()
-	if !isCreated then
-		vgui.Create("f4menu")
-		isCreated = true
-	end
-	
-	if !isVisible then
-		isVisible = true
-		gui.EnableScreenClicker(true)
-	else
-		isVisible = false
-		gui.EnableScreenClicker(false)
-	end
-end
-
-vgui.Register("f4menu", PANEL, "DPanel")
-
-net.Receive("TRP_F4Menu", function() 
-	jobstbl = net.ReadTable()
-	showMenu()
+TRP.AddF4MenuTab("Test #2", function (panel)
+	local button = vgui.Create("DButton", panel)
+	button:SetText("You Bitch")
+	button:SetSize(100, 30)
+	button:SetPos(200, 200)
 end)
--- gamemode/f4menu/server.lua
+++ gamemode/f4menu/server.lua
@@ -1,26 +1,4 @@
 function GM:ShowSpare2(ply)
-	net.Start("TRP_F4Menu")
-		local table = TRP.GetJobsTable()
-		if !table then
-			table = {}
-		end
-
-		net.WriteTable(table)
+	net.Start("TRP_OpenF4Menu")
 	net.Send(ply)
 end
-
-net.Receive("TRP_F4MenuSetJob", function(len, ply)
-	local category = net.ReadString()
-	local title = net.ReadString()
-	local rank = net.ReadString()
-
-	if ply:GetNWString("jobRank") != rank then
-		if TRP.SetPlayerJob(ply, category, title, rank) then
-			ply:PrintMessage(HUD_PRINTTALK, "You have successfully switched jobs to " .. title .. ", rank " .. rank)
-		else
-			ply:PrintMessage(HUD_PRINTTALK, "There was an error switching to that job!")
-		end		
-	else
-		ply:PrintMessage(HUD_PRINTTALK, "You are already set to that job!")
-	end
-end)
